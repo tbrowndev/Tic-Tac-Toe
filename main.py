e5c7cea52e2cf3bbda4da86eb72ebca6000ca3d9
Tommy Brown <46692709+tbrowndev@users.noreply.github.com>
2022-10-17 11:01:13 -0500

    Added comments to methods

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ class TicTacToe(object):
     self.spots = ['1','2','3','4','5','6','7','8','9']
     self.wins = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
     
+  
   def _markBoard(self, spot):
+    '''
+    Add player mark to board according to space selected.
+    TODO: Check for valid space input
+    '''
     while True:
       if self.spots[int(spot)-1]  == spot:
         self.spots[int(spot)-1] = self.players[self.player_turn]
@@ -17,14 +22,22 @@ class TicTacToe(object):
         selected_spot = input('Spot is taken choose another: ')
         spot = selected_spot
     
+
   def _setPlayerTurn(self):
+    '''
+    Set the next players turn
+    '''
     if self.player_turn == 0:
       self.player_turn = 1
     else:
       self.player_turn = 0
     print(f'Player {self.players[self.player_turn]}\'s turn')
     
+    
   def _checkWinner(self):
+    '''
+    Check for three of the same player marks in a row 
+    '''
     for i in self.wins:
       if self.spots[i[0]] == self.spots[i[1]] == self.spots[i[2]]:
         os.system('clear')
@@ -34,6 +47,9 @@ class TicTacToe(object):
     return False
     
   def _getBoard(self):
+    '''
+    Displays the board with (if any) player marks
+    '''
     print("\n")
     print("\t     |     |")
     print("\t  {}  |  {}  |  {}".format(self.spots[0], self.spots[1], self.spots[2]))
@@ -47,6 +63,9 @@ class TicTacToe(object):
     print()
     
   def playGame(self):
+    '''
+    Start the game
+    '''
     print('Lets play a game of Tic Tac Toe!')
     print('Players ready? ')
     prog = 0
